ecbe8292caf250102199325db71653fc27fb59a5
ChauWei200220 <65481625+ChauWei200220@users.noreply.github.com>
2021-01-03 14:20:01 +0700

    Fixed the login page

--- src/App.js
+++ src/App.js
@@ -1,41 +1,34 @@
 import './App.css';
 import imgLogo from './image/image.svg';
 import textLogo from './image/deviant-art-text.svg';
-// import leftLayout from './image/deviant-art.png';
 
 function App() {
   return (
     <div className="App">
-        <div className="leftLayout">
-          <div className="logo">
-            <img src={imgLogo}/>
-            <img src={textLogo}/>
-          </div>
-            <p className="Description">
-              JOIN THE LARGEST ART COMMUNITY IN THE WORLD
-            </p>
-            <p className="Note">
-              Explore and discover art, become a better artist, connect with others over mutual hobbies, or buy and sell work – you can do it all here.
-            </p>
-            
-          <div className="Creator">
-            <p className="artBy">
-              ART BY
-            </p>
-            <p className="nameCreator">
-              Li
-            </p>
-          </div>
-            
+      <div className="leftLayout">
+        <div className="logo">
+          <img src={imgLogo} />
+          <img src={textLogo} />
         </div>
-        <div className="rightLayout">
-          <p>
-            Login
+        <p className="description">
+          JOIN THE LARGEST ART COMMUNITY IN THE WORLD
+        </p>
+        <p className="note">
+          Explore and discover art, become a better artist, connect with others over mutual hobbies, or buy and sell work – you can do it all here.
+        </p>
+        <div className="creator">
+          <p className="editBy">
+            EDIT BY
           </p>
-        </div>
+          <p className="nameCreator">
+            Li
+          </p>
+        </div>    
+      </div>
+      <div className="rightLayout">
+        Login
+      </div>
     </div>
-    
   );
 }
-
 export default App;
